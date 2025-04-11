0d97525e82d3f438eaa64256b9757d47654ae9e9
孙娇 <P.ozzy@outlook.com>
2025-04-11 21:01:26 +0800

    some minor updates

--- src/similarity_clustering.py
+++ src/similarity_clustering.py
@@ -1,17 +1,18 @@
-import json
+import csv
 
 import numpy as np
 from scipy.cluster import hierarchy
 from scipy.cluster.hierarchy import to_tree
+from scipy.spatial.distance import squareform
 
-bird_info = "bird_info.json"
-with open(bird_info, 'r') as f:
-    data = f.read()
-
-bird_info = json.loads(data)
+bird_info = []
+with open("bird_info.csv", 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        bird_info.append(row)
 
 num_species = len(bird_info)
-labels = [f"{bird_info[i][2]}_{bird_info[i][0]}".replace(' ', '_') for i in range(num_species)]
+labels = [f"{i}_{bird_info[i][2]}_{bird_info[i][0]}".replace(' ', '_') for i in range(num_species)]
 similarity_matrix = np.loadtxt(
     "class_similarity.csv",
     delimiter=',',
@@ -20,11 +21,12 @@ similarity_matrix = np.loadtxt(
                   ))
 reduced_matrix = similarity_matrix[:num_species, :num_species]
 distance_matrix = 1 - reduced_matrix
+condensed_matrix = squareform(distance_matrix)
 
 # hierarchy clustering
-linkage_matrix = hierarchy.linkage(distance_matrix, method='average')
+linkage_matrix = hierarchy.linkage(condensed_matrix, method='average')
 
-# convert to newich file
+# convert to newick file
 def linkage_to_newick(matrix, labels):
     tree = to_tree(matrix, rd=False)
     def build_newick(node, parent_dist, leaf_names):
@@ -39,5 +41,5 @@ def linkage_to_newick(matrix, labels):
 newick_str = linkage_to_newick(linkage_matrix, labels)
 
 # save file
-with open("morphology_tree.tre", "w") as f:
+with open("morphology_cluster.tre", "w") as f:
     f.write(newick_str)
